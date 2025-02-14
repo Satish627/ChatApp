ae28322388cfe84a1307138b8c9ddce910075ca1
Satish <74795240+Satish627@users.noreply.github.com>
2025-02-14 15:28:21 +0100

    added user profile function

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,9 +1,9 @@
 import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js"
 import bcrypt from "bcryptjs"
+import cloudnary from "../lib/cloudnary.js";
 
 export const signup = async (req,res)=>{
-    console.log("Request Body:", req.body); // Log request body for debugging
 
     const {fullName,email, password } = req.body
     try{
@@ -49,9 +49,66 @@ export const signup = async (req,res)=>{
     }
 };
 
-export const login = (req,res)=>{
-    res.send("login route")
+export const login =async (req,res)=>{
+    const {email, password} = req.body
+
+    try{
+    const user  = await User.findOne({email});
+    if(!user){
+       return res.status(400).json({message: "Invalid credentials"})
+    }
+
+    const isPasswordCorrect = bcrypt.compare(password, user.password)
+    if(!isPasswordCorrect){
+        return res.status(400).json({message: "Invalid credentials"})
+    }
+    generateToken(user._id, res)
+    res.status(200).json({
+        id: user._id,
+        email:user.email,
+        fullName: user.fullName,
+        profilePic : user.profilePic
+    })
+
+    }
+    catch(error){
+        console.log("Error in login controller: ", error.message)
+        res.status(500).json({message:"Internal server error"})
+    }
 };
 export const logout = (req,res)=>{
-    res.send("loggout route")
+    try{
+        res.cookie("jwt", "", {maxAge:0})
+        res.status(200).json({message: "Logged out successfully"})
+    }
+    catch(error){
+
+        console.log("Error in logout controller: ", error.message)
+        res.status(500).json({message:"Internal server error"})
+    }
 };
+
+export const updateProfile =async (req,res)=>{
+    try{
+        const {profilePic} = req.body
+        const userId = req.user._id
+
+        if(!profilePic){
+            return res.status(400).json({message: "Profile pic is required"})
+        }
+
+        const uploadResponse = await cloudnary.uploader.upload(profilePic)
+        const updateUser = await User.findByIdAndUpdate(userId, {
+            profilePic: uploadResponse.secure_url
+        },
+        {
+            new:true
+        }
+    )
+    res.status(200).json(updateUser)
+    }
+    catch{
+        console.log("Error in update profile controller: ", error.message)
+        res.status(500).json({message:"Internal server error"})
+    }
+}
