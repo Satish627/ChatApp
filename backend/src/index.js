52264e97147dec093a5fb19a068ef6e359b6305a
Satish <74795240+Satish627@users.noreply.github.com>
2025-02-14 16:19:17 +0100

    added message parts as well

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv"
 import {connectDB} from "./lib/db.js"
 import cookieParser from "cookie-parser"
 import authRoutes from "./routes/auth.route.js"
+import messageRoutes from "./routes/message.route.js"
+
 
 dotenv.config()
 const app = express()
@@ -10,6 +12,7 @@ app.use(express.json())
 app.use(cookieParser())
 
 app.use("/api/auth", authRoutes)
+app.use("/api/message", messageRoutes)
 
 const PORT = process.env.PORT
 
