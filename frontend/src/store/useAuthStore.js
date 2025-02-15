30740e7b2d440c48d4cf9fe440321e188fbf7e89
Satish <74795240+Satish627@users.noreply.github.com>
2025-02-15 08:58:19 +0100

    login and logout added

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -4,7 +4,7 @@ import toast from "react-hot-toast";
 
 export const useAuthStore = create((set) => ({
     authUser: null,
-    isSigningUp: false, // ✅ Fixed typo (was `isSingingUp`)
+    isSigningUp: false, 
     isLoggingIn: false,
     isUpdatingProfile: false,
     isCheckingAuth: true,
@@ -20,9 +20,9 @@ export const useAuthStore = create((set) => ({
             set({ isCheckingAuth: false });
         }
     },
-
+   
     signup: async (data) => {
-        set({ isSigningUp: true }); // ✅ Fixed typo (was `isSingingUp`)
+        set({ isSigningUp: true }); 
         try {
             const res = await axiosInstance.post("/auth/signup", data);
             set({ authUser: res.data });
@@ -34,7 +34,22 @@ export const useAuthStore = create((set) => ({
             const errorMessage = error.response.data.message;
             toast.error(errorMessage);
         } finally {
-            set({ isSigningUp: false }); // ✅ Fixed typo
+            set({ isSigningUp: false }); 
+        }
+    },
+    login: async (data)=>{
+        set({isLoggingIn : true})
+        try{
+            const res = await axiosInstance.post("/auth/login", data);
+            set({authUser: res.data});
+            toast.success("Logged in successfully")
+
+        }
+        catch(error){
+            toast.error(error.response.data.message)
+        }
+        finally{
+            set({isLoggingIn: false})
         }
     },
     logout: async ()=>{
