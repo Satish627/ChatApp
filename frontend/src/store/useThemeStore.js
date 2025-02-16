77d7c42e4d164895da965a6ffb93dce36574ee26
Satish <74795240+Satish627@users.noreply.github.com>
2025-02-16 10:44:27 +0100

    working on setting ading themes

---
+++ frontend/src/store/useThemeStore.js
@@ -0,0 +1,10 @@
+import { Stethoscope } from "lucide-react"
+import {create} from "zustand"
+
+export const useThemeStore = create((set=>({
+    theme : localStorage.getItem("chat-theme") || "coffee",
+    setTheme: (theme)=>{
+        localStorage.setItem("chat-theme", theme);
+        set({theme})
+    }
+})))
